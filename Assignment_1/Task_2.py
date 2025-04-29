0c49fab781a092b392c5be1927aee90861dfbf64
Manpreet Singh <msingh@hpone.com>
2025-04-29 11:03:01 +0530

    Validation Added.

--- Assignment_1/Task_2.py
+++ Assignment_1/Task_2.py
@@ -2,8 +2,12 @@
 
 #Takes a user's first name and last name as input
 fName = input("Enter your first name: ")
+
 lName = input("Enter your second name: ")
 
-fullName = fName + " " + lName
+if fName.strip() != "" and lName.strip() != "":
+    fullName = fName + " " + lName
+    print("Hello, " + fullName + "! Welcome to the Python program.")
+else:
+    print("Error: First name and Last name cannot be empty.")
 
-print("Hello, " + fullName + "! Welcome to the Python program.")
